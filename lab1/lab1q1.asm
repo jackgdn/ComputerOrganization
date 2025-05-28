747dd26be70d8dc4d505e79f908784cea874097a
jackgdn <li_zhong_yao@foxmail.com>
2025-05-28 11:37:18 +0800

    docs: 修正注释格式

--- lab1/lab1q1.asm
+++ lab1/lab1q1.asm
@@ -1,4 +1,8 @@
-; You now implement the “Sub” operation. It is going to sub the values from the memory location stored at R3 from the values from the memory location stored at R2 (mem[R2] - mem[R3]). The result is saved at R1. Notice that you can only use R1 to R6 in your implementation. You also need to clear the values from R1 to R6 to ZERO before the end of your program.
+; You now implement the "Sub" operation.
+; It is going to sub the values from the memory location stored at R3 from the values from the memory location stored at R2 (mem[R2] - mem[R3]).
+; The result is saved at R1.
+; Notice that you can only use R1 to R6 in your implementation.
+; You also need to clear the values from R1 to R6 to ZERO before the end of your program.
 
 .ORIG X3000
 LDR R2, R2, #0
--- lab1/lab1q2.asm
+++ lab1/lab1q2.asm
@@ -1,4 +1,8 @@
-; You now implement the “XOR” operation. It is going to “XOR” the values from the memory location stored at R2 and the values from the memory location stored at R3 (mem[R2] XOR mem[R3]). The result is saved at R1. Notice that you can only use R1 to R6 in your implementation. You also need to clear the values from R1 to R6 to ZERO before the end of your program.
+; You now implement the "XOR" operation.
+; It is going to "XOR" the values from the memory location stored at R2 and the values from the memory location stored at R3 (mem[R2] XOR mem[R3]).
+; The result is saved at R1.
+; Notice that you can only use R1 to R6 in your implementation.
+; You also need to clear the values from R1 to R6 to ZERO before the end of your program.
 
 .ORIG X3000
 LDR R2, R2, #0
